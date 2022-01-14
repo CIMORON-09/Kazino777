292a127106c44f942028698400a591a59100cdc6
levon <airtk@ya.ru>
2022-01-14 18:32:16 +0300

    yuf

--- kazemno777.py
+++ kazemno777.py
@@ -1,62 +1,50 @@
 import random
 
 import wrap
-wrap.add_sprite_dir("fion")
-wrap.world.create_world(1000,800)
-fon=wrap.sprite.add("foi",500,400,"fon")
-fon=wrap.sprite.set_size(fon,1000,800)
-ooe = wrap.sprite.add_text( " " , 500, 300, text_color=(0, 255, 4), font_size=50)
 
-zifre=[1,2,3,4,5,6,7,8,9,10]
+wrap.add_sprite_dir("fion")
+wrap.world.create_world(1000, 800)
+fon = wrap.sprite.add("foi", 500, 400, "fon")
+fon = wrap.sprite.set_size(fon, 1000, 800)
+ooe = wrap.sprite.add_text(" ", 500, 300, text_color=(0, 255, 4), font_size=50)
+baraban_on = 0
+
+zifre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
 def kazinritapora():
     x = 200
 
-    wrap.sprite.add_text("МОНЕТЫ",80,25, text_color = (0, 255, 4), font_size =30)
+    wrap.sprite.add_text("МОНЕТЫ", 80, 25, text_color=(0, 255, 4), font_size=30)
 
-
-    wrap.sprite.add_text("10",180,25, text_color=(0, 255, 4), font_size=30)
-
+    wrap.sprite.add_text("10", 180, 25, text_color=(0, 255, 4), font_size=30)
 
     for did in zifre:
-        x+=50
+        x += 50
         if did == 10:
-            x+=20
-        wrap.sprite.add_text(str(did),x,650, text_color=(0, 123, 4),font_size=50,back_color=[255,12,0])
+            x += 20
+        wrap.sprite.add_text(str(did), x, 650, text_color=(0, 123, 4), font_size=50, back_color=[255, 12, 0])
 
 
-
-
-@wrap.always(delay=2)
+@wrap.always()
 def vibor_zifor():
+    if baraban_on == 1:
+        pervoe = zifre[1]
+        vtoroe = zifre[-1]
+        kol = random.randint(int(pervoe), int(vtoroe))
+
+        wrap.sprite_text.set_text(ooe, str(kol))
 
 
-        pervoe=zifre[1]
-        vtoroe=zifre[-1]
-        kol=random.randint(int(pervoe),int(vtoroe))
-
-        wrap.sprite_text.set_text(ooe,str(kol))
-        print(kol)
-
-
+@wrap.on_key_down(wrap.K_SPACE)
+def rererererere():
+    global baraban_on
+    if baraban_on == 0:
+        baraban_on = 1
+    else:
 
 
-
-
-
-# @wrap.on_key_down(wrap.K_p)
-# def vibor_zifor():
-#
-#
-#         pervoe=zifre[1]
-#         vtoroe=zifre[-1]
-#         kol=random.randint(int(pervoe),int(vtoroe))
-#
-#         wrap.sprite_text.set_text(ooe,str(kol))
-#         print(kol)
-
-
-
-
+        baraban_on = 0
 
 
 kazinritapora()
