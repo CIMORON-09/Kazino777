885baece56b30b243e450f430720f70db66b736f
levon <airtk@ya.ru>
2022-01-24 16:45:30 +0300

    21.01.2022

--- kazemno777.py
+++ kazemno777.py
@@ -10,14 +10,12 @@ ooe = wrap.sprite.add_text(" ", 500, 300, text_color=(0, 255, 4), font_size=50)
 baraban_on = 0
 gdkg = []
 zifre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+kolvomonet = 10
+vipavshee_chislo=None
 
 
 def kazinritapora():
     x = 200
-
-    wrap.sprite.add_text("МОНЕТЫ", 80, 25, text_color=(0, 255, 4), font_size=30)
-
-    wrap.sprite.add_text("10", 180, 25, text_color=(0, 255, 4), font_size=30)
 
     for did in zifre:
         x += 50
@@ -31,13 +29,14 @@ def kazinritapora():
 
 @wrap.always()
 def vibor_zifor():
+    global  vipavshee_chisl
     if baraban_on == 1:
-        pervoe = zifre[1]
-        vtoroe = zifre[-1]
-        kol = random.randint(int(pervoe), int(vtoroe))
+        pervoe = zifre[4]
+        vtoroe = zifre[4]
 
-        wrap.sprite_text.set_text(ooe, str(kol))
-
+        vipavshee_chislo = random.randint(int(pervoe), int(vtoroe))
+        wrap.sprite_text.set_text(ooe, str(vipavshee_chislo))
+        print(vipavshee_chislo)
 
 @wrap.on_key_down(wrap.K_SPACE)
 def rererererere():
@@ -52,14 +51,24 @@ def rererererere():
 @wrap.on_mouse_down(wrap.BUTTON_LEFT)
 def gyyggyggyg(pos_x, pos_y):
     for did in gdkg:
-        global old
+        global old,vipavshee_chislo,kolvomonet
         yre = wrap.sprite.is_collide_point(did, pos_x, pos_y)
+        kolvomonet_id=wrap.sprite.add_text(str(kolvomonet), 180, 25, text_color=(0, 255, 4), font_size=30)
+        wrap.sprite.add_text("МОНЕТЫ", 80, 25, text_color=(0, 255, 4), font_size=30)
         if yre == True:
-            new = did
 
-            wrap.sprite_text.set_text_color(new, 0, 0, 255)
-            wrap.sprite_text.set_text_color(old, 0, 255, 4)
-            old = new
+
+            wrap.sprite_text.set_text_color(did, 0, 0, 255)
+            wrap.sprite_text.set_text_color(old, 0, 123, 4)
+            old = did
+
+
+            if vipavshee_chislo==did :
+                kolvomonet+=5
+                wrap.sprite_text.set_text(kolvomonet_id,str(kolvomonet))
+
+
+
 
 
 kazinritapora()
